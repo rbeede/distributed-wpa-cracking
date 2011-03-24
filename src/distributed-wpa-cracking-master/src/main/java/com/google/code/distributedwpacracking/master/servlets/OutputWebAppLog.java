9325f6ef87c47fedb1b13ff430fadf6638390529
rodney.beede@gmail.com <rodney.beede@gmail.com@ab195588-0df4-aa2a-212f-7e42e2aa1cdd>
2011-03-24 15:40:59 +0000

    Fixed infinite loop bug in ssh remote command tool
    Added servlet to display the log4j log in the browser.

---
+++ src/distributed-wpa-cracking-master/src/main/java/com/google/code/distributedwpacracking/master/servlets/OutputWebAppLog.java
@@ -0,0 +1,56 @@
+package com.google.code.distributedwpacracking.master.servlets;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+
+public class OutputWebAppLog extends HttpServlet {
+	private static final long serialVersionUID = 8155994140355448770L;
+	
+	@Override
+	protected void doGet(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
+		final org.apache.log4j.rolling.RollingFileAppender rollingAppender = (org.apache.log4j.rolling.RollingFileAppender) Logger.getRootLogger().getAppender("rolling");
+		
+		if(null == rollingAppender) {
+			resp.sendError(HttpServletResponse.SC_NOT_FOUND, "No rolling log file is configured");
+			return;
+		}
+		
+		
+		final File logFile = new File(rollingAppender.getFile());
+		
+		if(null == logFile || !logFile.exists()) {
+			resp.sendError(HttpServletResponse.SC_NOT_FOUND, "No rolling log file exists");
+			return;
+		}
+		
+		
+		final BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(logFile), "UTF-8"));
+		
+		
+		resp.setContentType("text/plain");
+		resp.setCharacterEncoding("UTF-8");
+		resp.setStatus(HttpServletResponse.SC_OK);
+		
+		final PrintWriter writer = resp.getWriter();
+		
+		String cLine;
+		while(null != (cLine = reader.readLine())) {
+			writer.write(cLine);
+			writer.write(System.getProperty("line.separator"));
+		}
+		
+		reader.close();
+		
+	}
+}
