765e70e5a750530ac96949ec351980d06dc270f2
rrkroiss <rrkroiss@ab195588-0df4-aa2a-212f-7e42e2aa1cdd>
2011-04-20 16:36:29 +0000

    output solution file

--- src/cowpatty-4.6/cowpatty.c
+++ src/cowpatty-4.6/cowpatty.c
@@ -1332,7 +1332,7 @@ void* getCracking(void* arg) {
 		       solutionPath);
 	} else {
 	    logMessage(log_fd,"Solution file opened: %s\n", solutionPath);
-	    fwrite(passphrase, sizeof(passphrase),1,solutionFile);
+	    fwrite(&passphrase, sizeof(passphrase),1,solutionFile);
 	}
     }
     gettimeofday(&end, 0);
